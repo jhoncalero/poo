2f25745ca51aac28d1e020c0f196c8e03fd0b971
jhoncalero <jecalerog@gmail.com>
2016-11-11 13:00:24 -0500

    prueba1
    prueba1

---
+++ mensajeriaActualizar.php
@@ -0,0 +1,37 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Modificar Mensajeria</title>
+<link rel="shortcut icon" href="imagenes/iconosellos.ico">
+</head>
+<body>
+<style>
+body {
+background-image: url('./imagenes/fondo3.png');
+background-repeat: no-repeat;
+background-attachment: fixed;
+} 
+</style>
+<br><br>
+<table>
+<tr>
+<td>&emsp;&emsp;</td>
+<td>
+	<form action="mensajeriaActualizar2.php" method="post">
+	<h2 class="title" color="red"><FONT color="red"><em>“ Actualiza Datos Mensajeria "</em></FONT></a></h2>
+	CODIGO:
+	<input type="text" name="codigo">&thinsp;
+	<input type="submit" value="Buscar">
+	</form>
+</td>
+<td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</td>
+<td>
+	<form action="mensajeriaActualizar4.php" method="post">
+	<h2 class="title" color="red"><FONT color="red"><em>“ Anular Diligencia "</em></FONT></a></h2>
+	CODIGO:
+	<input type="text" name="codigo">&thinsp;
+	<input type="submit" value="Buscar">
+</form>
+</td>
+</body>
+</html>
