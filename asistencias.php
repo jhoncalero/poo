a5070091344fb864e7bf22d53ce561ef2a8c9710
jhoncalero <jecalerog@gmail.com>
2016-11-11 14:44:23 -0500

    dsdsdss
    sdsd

---
+++ asistencias.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<!-- Grids = http://www.w3schools.com/bootstrap/bootstrap_grid_basic.asp -->
+<div class="container-fluid">
+  <h1>INSTRUCTIVOS DE PAGO</h1>
+  <p>DEPARTAMENTO DE CARTERA</p>   
+
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>LIBERTY</h3>
+      <p>» Efectivo / Tarjeta de Crédito<BR>
+		» Pagos por <strong>PSE  www.libertycolombia.com.co</strong><BR>
+		» No se aceptan convenio sin respaldo de cheques<BR>
+		» Consignación <strong>Cta Ahorros CITIBANK N.5044814309</strong> a nombre de <strong>Liberty Seguros</strong> Enviar soporte de pago)<BR>
+		» Cheques girados a Liberty<BR>
+		» Financiación directa con la Cia. Pagare Financia Ya<BR>
+		» Financiación se debe legalizar antes de los 30 días<BR>
+		» El pago en la compañía debe de ser registrado antes de los 30 días</p>
+    </div>
+    <div class="col-sm-4">
+     <h3>SURAMERICANA</h3>        
+     <p>» Efectivo / Tarjeta de Crédito<BR>
+		» No se aceptan convenio de pago<BR>
+		» Pagos <strong>PSE (www.sura.com)</strong> Pago exxpress<BR>
+		» Cheque al día Girado a nombre de Seguros Generales Suramericana<BR>
+		» <strong>Financiación directa con la Cia. (poliza pago  mensual)</strong><BR>
+		» Financiación se debe legalizar antes de los 30 días<BR>
+		» El pago en la compañía debe de ser registrado antes de los 30 días<BR>
+		» Toda póliza que supera los 30 días, el cliente debe de firmar una carta de no siniestro para ampliar la fecha de pago.
+	</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>AXA COLPATRIA</h3>
+      <p>» Efectivo / Tarjeta de Crédito<BR>
+		» Pagos <strong>PSE (www.axacolpatria.co)</strong><BR>
+		» No se aceptan convenio de pago<BR>
+		» Consignación <strong>Cta Cte  N.0121072874</strong> a nombre de <strong>Seguros Colpatria</strong> (enviar soporte)<BR>
+		» Cheque al día girado a Seguros Colpatria S.A.<BR>
+		» Financiación directa con la Cia. Colpatria<BR>
+		» Financiación se debe legalizar  antes de los 30 días<BR>
+		» El pago en la compañía debe de ser registrado antes de los 30 días<BR>
+		» Toda póliza que supera los 30 días, el cliente debe de firmar una carta de no siniestro para ampliar la fecha de pago.
+</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>SEGUROS DEL ESTADO</h3>
+      <p>» Efectivo / Tarjeta de Crédito<BR>
+		 » Pago directo con el código de barras de la póliza.<BR>
+		 » Pago Audio Estado (tarjeta de crédito)<BR>
+		 » Pago por <strong>PSE (www.segurosdelestado.com)</strong><BR>
+		 » Cheques Girados a Seguros del Estado<BR>
+		 » Convenio pago al Dia 30 y 45<BR>
+		 » Pagare <strong>CREDIVALORES,PICHINCHA, FINESA Y EXXTRA.</strong><BR>
+		 » Financiación directa con la Cia. (San Germán)<BR>
+	     » Financiación se debe legalizar  antes de los 30 días<BR>
+	     » El pago en la compañía debe de ser registrado antes de los 30 días<BR>
+	     » Toda póliza que supera los 30 días, el cliente debe de firmar una carta de no siniestro y solicitar autorización a la compañía.
+</p>
+    </div>
+	 <div class="col-sm-4">
+     <h3>LA EQUIDAD</h3>        
+     <p>» Efectivo / Tarjeta de Crédito<BR>
+		» Pagos <strong>PSE (www.laequidadseguros.coop)</strong> servicios en  línea<BR>
+		» Consignación <strong>Cuenta Cte Banco de Bogotá No.511067290</strong> a nombre de  <strong>LA EQUIDAD SEGUROS</strong><BR>
+		» Convenio al día 30 y 45 soportado con cheque (con autorización de la Cia. el valor de la póliza debe ser superior a 1.500.000)<BR>
+		» Pagare <strong>PICHINCHA y EXXTRA TOMADOR</strong><BR>
+		» Financiación se debe legalizar  antes de los 30 días<BR>
+		» El pago en la compañía debe de ser registrado antes de los 30 días<BR>
+		» Toda póliza que supera los 30 días, el cliente debe de firmar una carta de no siniestro y solicitar autorización a la compañía.
+	</p>
+    </div>
+  </div>
+  
+  
+    <div class="row">
+    <div class="col-sm-4">
+      <h3>LA PREVISORA</h3>
+      <p>» Efectico / Tarjeta de Crédito<BR>
+		» No se aceptan convenio de pago<BR>
+		» Pagos <strong>PSE (www.previsora.gov.co)</strong><BR>
+		» Cheques Girados a nombre de LA PREVISORA (el monto de cada cheque debe ser superior a $515.000).<BR>
+		» Pagare <strong>FINESA, CREDIVALORES, PICHINCHA Y EXXTRA (Tomador)</strong><BR>
+		» Financiación directa con la Cia. sin recargos (Casos especiales)<BR>
+		» El pago en la compañía debe de ser registrado antes de los 30 días
+		</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>SEGUROS BOLIVAR</h3>
+      <p>» Efectivo / Tarjeta de Crédito<BR>
+		» Pagos <strong>PSE (www.segurosbolivar.co)</strong> pagos en línea<BR>
+		» Consignación Cuenta Corriente Davivienda 006069999644 a nombre de SEGUROS COMERCIALES BOLIVAR<BR>
+		» Pago con cheque al día girado a nombre de SEGUROS COMERCIALES BOLIVAR.<BR>
+		» Financiación <strong>CREDIVALORES y EXXTRA (Tomador)</strong><BR>
+		» Financiación se debe legalizar  antes de los 6 días<BR>
+		» El pago en la compañía debe de ser registrado antes de los 6 días<BR>
+		» Si se realiza convenio debe ser dentro de los 25 días contando desde el inicio de vigencia
+	</p>
+    </div>
+	 <div class="col-sm-4">
+     <h3>ASEGURADORA SOLIARIA</h3>        
+     <p>» Efectivo<BR>
+		» Pagos <strong>PSE (www.aseguradorasolidaria.com.co)</strong> Multipago Solidaria<BR>
+		» Convenio al día 30 y 45 (cheques o efectivo)<BR>
+		» Financiación <strong>PICHINCHA</strong><BR>
+		» Financiación se debe legalizar antes de los 30 días<BR>
+		» El pago en la compañía debe de ser registrado antes de los 30 días<BR>
+		» Toda póliza que supera los 30 días, el cliente debe de firmar una carta de no siniestro y solicitar autorización a la compañía.
+	</p>
+    </div>
+  </div>
+  
+    
+    <div class="row">
+    <div class="col-sm-4">
+      <h3>GENERALI</h3>
+      <p>» Efectivo<BR>
+		» Pagos <strong>PSE (www.generali.com.co)</strong> Pagos pse<BR>
+		» Convenio especial  al día 30 y 45 (se debe confirmar modalidad antes de emitir póliza)<BR>
+		» Financiación <strong>CREDIVALORES</strong><BR>
+		» Financiación se debe legalizar  antes de los 30 días<BR>
+		» El pago en la compañía debe de ser registrado antes de los 30 días<BR>
+		» Toda póliza que supera los 30 días, el cliente debe de firmar una carta de no siniestro y solicitar  autorización  a la  compañía.
+		</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>MAPFRE</h3>
+      <p>» Efectivo / Tarjeta de Crédito<BR>
+		» Pagos <strong>PSE (www.mapfre.com.co)</strong> por clientes<BR>
+		» Convenio especial al día 30 y 60 (con  cheques posfechados a nombre de Mapfre seguros)<BR>
+		» Pago con cheque al día girado a nombre de Mapfre Seguros.<BR>
+		» Financiación <strong>CREDIMAPFRE</strong><BR>
+		» Financiación se debe legalizar antes de los 30 días<BR>
+		» El pago en la compañía debe de ser registrado antes de los 30 días<BR>
+		» Toda póliza que supera los 30 días, el cliente debe de firmar una carta de no siniestro y solicitar autorización a la compañía.
+	</p>
+    </div>
+  </div>
+  
+
+</div>
+<BR>
+<center><img src="imagenes/asistencias.jpg" style="width:1100px;height:600px;"></center><BR>
+</body>
+</html>
